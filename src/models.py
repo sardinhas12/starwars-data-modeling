6b5f94c4fbf1df6cc2f6173cebd08a5ad5364310
sardinhas12 <patriciasardinharocha@gmail.com>
2022-08-16 19:12:59 +0000

    commit

--- src/models.py
+++ src/models.py
@@ -7,24 +7,47 @@ from sqlalchemy import create_engine
 from eralchemy import render_er
 
 Base = declarative_base()
-
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__= 'user'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250))
+    user_name = Column(String(250))
+    email = Column(String(250))
+    password = Column(String(250))
+   
+class Favorite(Base):
+    __tablename__ = 'favorite'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_id=Column(Integer, ForeignKey('user.id'))
+    character_id = Column(String(250), ForeignKey('character.id'))
+    planet_id = Column(String(250), ForeignKey('planet.id'))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Character (Base):
+    __tablename__ = 'character'
+    id = Column(String(250), primary_key = True)
+    name = Column(String(250))
+    height = Column(Integer)
+    mass = Column(Integer)
+    birth_year = Column(Integer)
+    gender = Column(String(250))
+    eye_color = Column(String(250))
+    skin_color = Column(String(250))
+    hair_color = Column(String(250))
+    uid = Column(Integer)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(String(250), primary_key=True)
+    name = Column(String(250))
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    climate = Column(String(250))
+    terrain = Column(String(250))
+    gravity = Column(String (250)) 
+    diameter = Column(Integer)
+    population = Column(Integer) 
+    surface_water = Column(Integer)
+    uid = Column(Integer)
 
     def to_dict(self):
         return {}
